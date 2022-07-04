19c9d084359121b39f4ae99a1e6b80ff77626167
Swan BENOIT <swan.benoit@gmail.com>
2022-07-04 14:35:35 +0200

    add pagination button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Pokemons from './components/Pokemons/Pokemons';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="pokemon-container">
-        <Pokemons page={0} size={20}></Pokemons>
-      </div>
-      <div className="pagination">
+    const [page, setPage] = useState(0);
 
+    return (
+    <div className="App">
+        <div className="pagination">
+            {page > 0 && <button onClick={() => setPage(page - 1)}>PREV</button>}
+            <button onClick={() => setPage(page + 1)}>NEXT</button>
+        </div>
+        <div className="pokemon-container">
+        <Pokemons page={page} size={20}></Pokemons>
       </div>
     </div>
   );
